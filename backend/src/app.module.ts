025980743dd0d7551bcbf5165ee93d313028b854
Netanel Shpakov <netanelshpakov@Netanels-MacBook-Air.local>
2026-04-07 21:53:21 +0300

    Connected the backend to the database

---
+++ backend/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        dialect: 'postgres',
+        host: config.get<string>('DB_HOST', 'localhost'),
+        port: config.get<number>('DB_PORT', 5432),
+        username: config.get<string>('DB_USER', 'postgres'),
+        password: config.get<string>('DB_PASS', ''),
+        database: config.get<string>('DB_NAME', ''),
+        autoLoadModels: true,
+        synchronize: false,
+      }),
+    }),
+  ],
+})
+
+export class AppModule { }
